886842cb1be12006527ee67744d1c7bd46de8c5c
Cathy Diaz <Cathydiaz14@gmail.com>
2021-02-10 15:29:40 -0800

    commit tree acee4bb751030d1543712c310ac8ca5d6d41038e

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+
+def prime?(number)
+    if number <= 1
+        return false
+    else
+        (2..(number-1)).each {|i| return false if number % i == 0}
+    end
+    true
+end
+
+
+
